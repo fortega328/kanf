c3dde44346a0caa1fef8013a88f085191fc33f55
Kathy Rodriguez <katrrodriguez26@gmail.com>
2021-05-28 21:22:37 -0400

    Update personalquestions.jsx

---
+++ client/src/components/Questions/personalquestions.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+
+function Questions() {
+	const personalquestions= [
+		{
+            "question": "Who are the most important people in your life?", 
+            "answer": ""
+        },
+        {
+            "question": "Where is ‘home’?", 
+            "answer": "" 
+        },
+        {
+            "question": "Where is ‘home’?", 
+            "answer": ""   
+        },
+        {
+            "question": "What’s your big passion?", 
+            "answer":""
+        },
+        {
+            "question": "Do you read reviews, or just go with your gut?", 
+            "answer":"" 
+        },
+        {
+            "question": "What’s the best meal you’ve ever had?", 
+            "answer":""  
+        },
+        {
+            "question": "What are your hobbies, and how did you get into them?", 
+            "answer": ""
+        },
+        {
+            "question": "Would you say you’re more of an extrovert or an introvert?", 
+            "answer":""
+        },
+        {
+            "question": "If you had a free plane ticket, where would you like to go?", 
+            "answer":""
+        },
+        {
+            "question": "What type of music are you into?", 
+            "answer":"" 
+        },
+        {
+            "question": "What toppings are needed to create the perfect burger?", 
+            "answer":"" 
+        },
+    ];
